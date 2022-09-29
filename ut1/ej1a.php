78eecf4369e7698f43e93827500b40d14ff72f04
DianaD6 <113586290+DianaD6@users.noreply.github.com>
2022-09-29 09:58:28 +0200

    Update ej1a.php

--- ut1/ej1a.php
+++ ut1/ej1a.php
@@ -41,3 +41,43 @@ $posicion++;
 ?>
 </BODY>
 </HTML>
+
+
+V2.0
+
+<HEAD><TITLE> Ejercicios arrays unidimensionales </TITLE></HEAD>
+<BODY>
+<?php
+echo "<table border='2'>";
+echo"	<tr>";
+echo"		<th>Media impares</th>";
+echo"		<th>Media pares</th>";
+echo"	</tr>";
+$i=1;
+$impar=array();
+while(count($impar)<20){
+	
+	if($i%2  !=0){
+		array_push($impar, $i);
+	}
+	$i++;
+}
+$sumaImpar = 0;
+$sumaPar = 0;
+for($i=0; $i < count($impar); $i++){
+	
+	if($i%2 != 0){
+		$sumaImpar=$sumaImpar+$impar[$i];
+	}else{
+		$sumaPar=$sumaPar+$impar[$i];
+	}
+}
+$mediaImpares = $sumaImpar/(count($impar)/2);
+$mediaPares = $sumaPar/(count($impar)/2);
+echo "<tr>";
+echo	"<td>$mediaImpares</td>";
+echo	"<td>$mediaPares</td>";
+echo "</tr>";
+?>
+</BODY>
+</HTML>
