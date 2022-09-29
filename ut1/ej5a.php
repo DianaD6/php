a04765d4d8f754b13ed5a29e28781b5641f9b649
DianaD6 <113586290+DianaD6@users.noreply.github.com>
2022-09-29 19:55:11 +0200

    Update ej5a.php

--- ut1/ej5a.php
+++ ut1/ej5a.php
@@ -1,3 +1,4 @@
+
 <HTML>
 
 <HEAD>
@@ -6,10 +7,19 @@
 
 <BODY>
     <?php
-  
+    /*definir tres arrays con los siguientes valores relativos a módulos que se imparten en
+el ciclo DAW:
+“Bases Datos”, “Entornos Desarrollo”, “Programación”
+“Sistemas Informáticos”,”FOL”,”Mecanizado”
+“Desarrollo Web ES”,”Desarrollo Web EC”,”Despliegue”,”Desarrollo Interfaces”, “Inglés”
+Se pide:
+*/
+
     $arr1 = array("Bases Datos", "Entornos Desarrollo", "Programación");
     $arr2 = array("Sistemas Informaticos", "FOL", "Mecanizado");
     $arr3 = array("Desarrollo Web ES", "Desarrollo Web EC", "Despliegue", "Desarrollo Interfaces", "Ingles");
+
+    //a. Unir los 3 arrays en uno único sin utilizar funciones de arrays
     $arr4 = array();
 
     for ($i = 0; $i < count($arr1); $i++) {
@@ -26,12 +36,19 @@
     }
 
     var_dump($arr4);
-  
-  
-    // foreach ($arr4 as $valor) {
-    //     echo $valor . "</br>";
-    // }
 
+    //b. Unir los 3 arrays en uno único usando la función array_merge()
+    $arrMer = array_merge($arr1, $arr2, $arr3);
+    var_dump($arrMer);
+
+    //c. Unir los 3 arrays en uno único usando la función array_push()
+    foreach ($arr1 as $value) {
+        array_push($arr2, $value);
+    }
+    foreach ($arr3 as $value) {
+        array_push($arr2, $value);
+    }
+    var_dump($arr2);
     ?>
 </BODY>
 
